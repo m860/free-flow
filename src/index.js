75718c22f105a494ef1a7d870b6dee68fa53e2dc
jean <jean.ma.1986@gmail.com>
2017-09-05 10:03:30 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import request from 'request'
+import log4js from 'log4js'
+
+log4js.configure({
+	appenders: {sign: {type: 'file', filename: 'sign.log'}},
+	categories: {default: {appenders: ['sign'], level: 'all'}}
+});
+
+const log = log4js.getLogger("sign");
 
 const sign = (ssocookie)=> {
+	log.info(`start sign ${ssocookie}`)
 	return new Promise((resolve, reject)=> {
 		request.post('http://218.205.252.24:18081/scmccCampaign/signCalendar/sign.do', {
 			SSOCookie: ssocookie
 		}, (err, res, body)=> {
 			if (err) {
-				reject(err);
+				log.error(err);
+				resolve();
 			}
 			else {
-				const result = JSON.parse(body).result;
-				if (result.code === 0) {
-					//success
-					resolve('signed success')
+				const contentType = res.headers['content-type'];
+				if (/application\/json/.test(contentType)) {
+					try {
+						const result = JSON.parse(body).result;
+						if (result.code === 0) {
+							//success
+							log.info('sign success!', body);
+							resolve()
+						}
+						else {
+							log.info('sign fail!', body);
+							resolve();
+						}
+					}
+					catch (ex) {
+						log.error(ex, body);
+					}
 				}
 				else {
-					resolve(`signed fail.result=${JSON.stringify(result)}`)
+					log.info(body);
 				}
 			}
 		});
@@ -24,15 +47,21 @@ const sign = (ssocookie)=> {
 }
 
 const ssocookies = [
-	"6435FA9EC9F6A64B105276D0223D129D"
+	"6435FA9EC9F6A64B105276D0223D129D",
+	"8259AC0813A148B869AC6EF551FF2C5A"
 ];
 
-ssocookies.map(async(cookie)=> {
-	try {
-		const message = await sign(cookie);
-		console.log(message);
+async function run(i = 0) {
+	const len = ssocookies.length;
+	if (i < len) {
+		const cookie = ssocookies[i];
+		await sign(cookie);
+		await run(i + 1);
 	}
-	catch (ex) {
-		console.error(ex);
-	}
-});
+}
+
+run()
+
+// ssocookies.forEach(async(cookie)=> {
+// 	await sign(cookie);
+// });
