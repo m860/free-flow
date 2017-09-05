ddc28c140b6637a009852348e0cbc5833f6c05bd
jean <jean.ma.1986@gmail.com>
2017-09-05 10:13:35 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -11,8 +11,15 @@ const log = log4js.getLogger("sign");
 const sign = (ssocookie)=> {
 	log.info(`start sign ${ssocookie}`)
 	return new Promise((resolve, reject)=> {
-		request.post('http://218.205.252.24:18081/scmccCampaign/signCalendar/sign.do', {
-			SSOCookie: ssocookie
+		request({
+			url: "http://218.205.252.24:18081/scmccCampaign/signCalendar/sign.do",
+			method: "POST",
+			headers: {
+				"User-Agent": "Mozilla/5.0 (Linux; Android 7.0; BLN-AL10 Build/HONORBLN-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36/3.4.0 scmcc.mobile"
+			},
+			formData: {
+				SSOCookie: ssocookie
+			}
 		}, (err, res, body)=> {
 			if (err) {
 				log.error(err);
