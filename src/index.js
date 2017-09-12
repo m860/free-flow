6582a762ba765afedab3b73d65aae8021143d9cb
jean <jean.ma.1986@gmail.com>
2017-09-12 12:13:08 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -113,7 +113,7 @@ const querySignStatus = (name, SSOCookie, JSESSIONID)=> {
 	});
 }
 
-const getSignPrize = (name, SSOCookie, JSESSIONID, type)=> {
+const getSignPrize = (name, SSOCookie, JSESSIONID, WT_FPC, type)=> {
 	log.info(`start get prize ${name}`);
 	return new Promise((resolve, reject)=> {
 		request({
@@ -121,12 +121,10 @@ const getSignPrize = (name, SSOCookie, JSESSIONID, type)=> {
 			method: "POST",
 			headers: {
 				...defaultHeaders,
+				//Referer: "http://218.205.252.24:18081/scmccCampaign/signCalendar/index.html?abStr=6124",
 				"Cookie": `cstamp=1504577034130; smsCityCookie=11; SmsNoPwdLoginCookie=${SSOCookie}; tokenid=; SSOCookie=; JSESSIONID=${JSESSIONID};`,
 			},
-			formData: {
-				SSOCookie: SSOCookie,
-				type: type
-			}
+			body: `SSOCookie=${SSOCookie}&type=${type}`
 		}, (err, res, body)=> {
 			if (err) {
 				log.error(err);
@@ -140,57 +138,16 @@ const getSignPrize = (name, SSOCookie, JSESSIONID, type)=> {
 	})
 }
 
-/*
- const request10086 = (data)=> {
- const ssocookie = data.cookie;
- const name = data.name;
- log.info(`start ${getActionName(data.action)} ${name}`)
- return new Promise((resolve, reject)=> {
- let url = data.action;
- if (url === action.lottery) {
- url += Math.random();
- }
- let formData = {
- SSOCookie: ssocookie
- };
- if (url === action.sevenDay) {
- formData.type = 1;
- }
- request({
- url: url,
- method: "POST",
- headers: {
- "Accept-Language": "zh-CN,en-US;q=0.8",
- "Content-Type": "application/x-www-form-urlencoded",
- "Cookie": `cstamp=1504577034130; smsCityCookie=11; SmsNoPwdLoginCookie=${ssocookie}; tokenid=; SSOCookie=; JSESSIONID=${data.JSESSIONID};`,//WT_FPC=id=2218aa09807ed562c0b1504575212738:lv=1504664053259:ss=1504663773358
- "User-Agent": "Mozilla/5.0 (Linux; Android 7.0; BLN-AL10 Build/HONORBLN-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36/3.4.0 scmcc.mobile"
- },
- formData: formData
- }, (err, res, body)=> {
- if (err) {
- log.error(err);
- resolve();
- }
- else {
- log.info(body);
- resolve();
- }
- });
- })
-
- }
- */
-
 const userConfiguration = [
 	{
 		name: "walle",
 		SSOCookie: "6435FA9EC9F6A64B105276D0223D129D",
-		JSESSIONID: "2x5U9yCPqSRjrChOXUO6gy1osMD3WwpYznCtPhBY2MxDj2_RoAT7!3057625",
+		JSESSIONID: "2x5U9yCPqSRjrChOXUO6gy1osMD3WwpYznCtPhBY2MxDj2_RoAT7!3057625"
 	},
 	{
 		name: "steve",
 		SSOCookie: "8259AC0813A148B869AC6EF551FF2C5A",
-		JSESSIONID: "Il1U_amdYdKfgbthGGwkeTphXFF6Ws8ZQN7ED5S8C9P2h1qLQZO8!1178766403",
+		JSESSIONID: "clx0OGNuTCqz6mPvj5I7tFLV3c4ZdAZnJ7r38QvCI_DjEe4NvIt6!1438695398"
 	},
 ];
 
