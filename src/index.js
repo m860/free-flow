6fcc48853121476a032a276802752c3fa61cc142
jean <jean.ma.1986@gmail.com>
2017-12-08 09:56:25 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -31,20 +31,28 @@ function getActionName(value) {
 	return 'unknown';
 }
 
-export const sign = (name, SSOCookie, JSESSIONID)=> {
+export const sign = (name, SSOCookie, JSESSIONID,) => {
+	let cookies = [
+		`SmsNoPwdLoginCookie=${SSOCookie}`,
+		`smsCityCookie=11`,
+		`tokenid=`,
+		`SSOCookie=`,
+		`JSESSIONID=${JSESSIONID}`
+	];
+
 	log.info(`start sign ${name}`);
-	return new Promise((resolve, reject)=> {
+	return new Promise((resolve, reject) => {
 		request({
 			url: 'http://218.205.252.24:18081/scmccCampaign/signCalendar/sign.do',
 			method: "POST",
 			headers: {
 				...defaultHeaders,
-				"Cookie": `cstamp=${Date.now()}; smsCityCookie=11; SmsNoPwdLoginCookie=${SSOCookie}; tokenid=; SSOCookie=; JSESSIONID=${JSESSIONID};`,
+				"Cookie": cookies.join(';'),
 			},
 			formData: {
 				SSOCookie: SSOCookie
 			}
-		}, (err, res, body)=> {
+		}, (err, res, body) => {
 			if (err) {
 				log.error(err);
 				resolve();
@@ -57,20 +65,27 @@ export const sign = (name, SSOCookie, JSESSIONID)=> {
 	})
 };
 
-export const lottery = (name, SSOCookie, JSESSIONID)=> {
+export const lottery = (name, SSOCookie, JSESSIONID) => {
+	let cookies = [
+		`SmsNoPwdLoginCookie=${SSOCookie}`,
+		`smsCityCookie=11`,
+		`tokenid=`,
+		`SSOCookie=`,
+		`JSESSIONID=${JSESSIONID}`
+	];
 	log.info(`start lottery ${name}`)
-	return new Promise((resolve, reject)=> {
+	return new Promise((resolve, reject) => {
 		request({
 			url: `http://218.205.252.24:18081/scmccCampaign/dazhuanpan/dzpDraw.do?t=${Math.random()}`,
 			method: "POST",
 			headers: {
 				...defaultHeaders,
-				"Cookie": `cstamp=${Date.now()}; smsCityCookie=11; SmsNoPwdLoginCookie=${SSOCookie}; tokenid=; SSOCookie=; JSESSIONID=${JSESSIONID};`,
+				"Cookie": cookies.join(';'),
 			},
 			formData: {
 				SSOCookie: SSOCookie
 			}
-		}, (err, res, body)=> {
+		}, (err, res, body) => {
 			if (err) {
 				log.error(err);
 				resolve();
@@ -83,19 +98,26 @@ export const lottery = (name, SSOCookie, JSESSIONID)=> {
 	})
 }
 
-export const querySignStatus = (name, SSOCookie, JSESSIONID)=> {
-	return new Promise((resolve, reject)=> {
+export const querySignStatus = (name, SSOCookie, JSESSIONID) => {
+	let cookies = [
+		`SmsNoPwdLoginCookie=${SSOCookie}`,
+		`smsCityCookie=11`,
+		`tokenid=`,
+		`SSOCookie=`,
+		`JSESSIONID=${JSESSIONID}`
+	];
+	return new Promise((resolve, reject) => {
 		request({
 			url: `http://218.205.252.24:18081/scmccCampaign/signCalendar/queryPrizeAndDrawStatus.do`,
 			method: "POST",
 			headers: {
 				...defaultHeaders,
-				"Cookie": `cstamp=${Date.now()}; smsCityCookie=11; SmsNoPwdLoginCookie=${SSOCookie}; tokenid=; SSOCookie=; JSESSIONID=${JSESSIONID};`,
+				"Cookie": cookies.join(';'),
 			},
 			formData: {
 				SSOCookie: SSOCookie
 			}
-		}, (err, res, body)=> {
+		}, (err, res, body) => {
 			if (err) {
 				log.error(err);
 				resolve();
@@ -115,19 +137,26 @@ export const querySignStatus = (name, SSOCookie, JSESSIONID)=> {
 	});
 }
 
-export const getSignPrize = (name, SSOCookie, JSESSIONID, type)=> {
+export const getSignPrize = (name, SSOCookie, JSESSIONID, type) => {
+	let cookies = [
+		`SmsNoPwdLoginCookie=${SSOCookie}`,
+		`smsCityCookie=11`,
+		`tokenid=`,
+		`SSOCookie=`,
+		`JSESSIONID=${JSESSIONID}`
+	];
 	log.info(`start get prize ${name}`);
-	return new Promise((resolve, reject)=> {
+	return new Promise((resolve, reject) => {
 		request({
 			url: `http://218.205.252.24:18081/scmccCampaign/signCalendar/draw.do`,
 			method: "POST",
 			headers: {
 				...defaultHeaders,
 				//Referer: "http://218.205.252.24:18081/scmccCampaign/signCalendar/index.html?abStr=6124",
-				"Cookie": `cstamp=${Date.now()}; smsCityCookie=11; SmsNoPwdLoginCookie=${SSOCookie}; tokenid=; SSOCookie=; JSESSIONID=${JSESSIONID};`,
+				"Cookie": cookies.join(';'),
 			},
 			body: `SSOCookie=${SSOCookie}&type=${type}`
-		}, (err, res, body)=> {
+		}, (err, res, body) => {
 			if (err) {
 				log.error(err);
 				resolve();
@@ -144,7 +173,7 @@ const userConfiguration = [
 	{
 		name: "walle",
 		SSOCookie: "6435FA9EC9F6A64B105276D0223D129D",
-		JSESSIONID: "_QK-RoScK0GUgaZZHxB1fqj6Dcxdom_AgwDIAETxviOxcDlJeBsQ!-994573411"
+		JSESSIONID: "lAEz0hPECqnrUDQIrg8lZXYVMHuCWAPbmCtbaXbgKcnuWCQ7uJrq!-522203662"
 	},
 	{
 		name: "steve",
@@ -163,29 +192,29 @@ async function run(i = 0) {
 		const opt = userConfiguration[i];
 		await sign(opt.name, opt.SSOCookie, opt.JSESSIONID);
 		await lottery(opt.name, opt.SSOCookie, opt.JSESSIONID);
-		if (opt.name === 'walle') {
-			const signStatus = await querySignStatus(opt.name, opt.SSOCookie, opt.JSESSIONID);
-			if (signStatus) {
-				if (signStatus.result) {
-					if (signStatus.result.obj) {
-						const day = signStatus.result.obj.dayNum;
-						log.info(`sign ${day} day`);
-						if (day === 1) {
-							await getSignPrize(opt.name, opt.SSOCookie, opt.JSESSIONID, 0)
-						}
-						if (day === 7) {
-							await getSignPrize(opt.name, opt.SSOCookie, opt.JSESSIONID, 1)
-						}
-						if (day === 15) {
-							await getSignPrize(opt.name, opt.SSOCookie, opt.JSESSIONID, 2)
-						}
-						if (day === 25) {
-							await getSignPrize(opt.name, opt.SSOCookie, opt.JSESSIONID, 3)
-						}
-					}
-				}
-			}
-		}
+		// if (opt.name === 'walle') {
+		// 	const signStatus = await querySignStatus(opt.name, opt.SSOCookie, opt.JSESSIONID);
+		// 	if (signStatus) {
+		// 		if (signStatus.result) {
+		// 			if (signStatus.result.obj) {
+		// 				const day = signStatus.result.obj.dayNum;
+		// 				log.info(`sign ${day} day`);
+		// 				if (day === 1) {
+		// 					await getSignPrize(opt.name, opt.SSOCookie, opt.JSESSIONID, 0)
+		// 				}
+		// 				if (day === 7) {
+		// 					await getSignPrize(opt.name, opt.SSOCookie, opt.JSESSIONID, 1)
+		// 				}
+		// 				if (day === 15) {
+		// 					await getSignPrize(opt.name, opt.SSOCookie, opt.JSESSIONID, 2)
+		// 				}
+		// 				if (day === 25) {
+		// 					await getSignPrize(opt.name, opt.SSOCookie, opt.JSESSIONID, 3)
+		// 				}
+		// 			}
+		// 		}
+		// 	}
+		// }
 		await run(i + 1);
 	}
 }
